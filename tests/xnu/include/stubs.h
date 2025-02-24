f89990d351f6eafe87770fdc55c039f49edbd165
Muirey03 <tcmuir03@gmail.com>
2025-02-24 23:05:03 +0000

    Add stubs header for XNU analysis

---
+++ tests/xnu/include/stubs.h
@@ -0,0 +1,15 @@
+#ifndef STUBS_H
+#define STUBS_H
+
+#include <pexpert/arm64/board_config.h>
+
+#undef CPU_HAS_APPLE_PAC
+#undef HAS_APPLE_PAC
+#define CONFIG_ROSETTA 1
+
+#define __builtin_xnu_type_signature(...) "12"
+#define __builtin_xnu_type_summary(...) 0
+#define __builtin_xnu_types_compatible(...) 1
+
+
+#endif
