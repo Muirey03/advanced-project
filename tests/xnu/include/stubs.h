4712d958a37a5374bc67a937ba3db2bcb437b192
Muirey03 <tcmuir03@gmail.com>
2025-02-24 23:18:20 +0000

    Annotate XNU entrypoints

--- tests/xnu/include/stubs.h
+++ tests/xnu/include/stubs.h
@@ -7,9 +7,10 @@
 #undef HAS_APPLE_PAC
 #define CONFIG_ROSETTA 1
 
+#define THREAD_ENTRY __attribute__((annotate("thread_entrypoint")))
+
 #define __builtin_xnu_type_signature(...) "12"
 #define __builtin_xnu_type_summary(...) 0
 #define __builtin_xnu_types_compatible(...) 1
-
 
 #endif
