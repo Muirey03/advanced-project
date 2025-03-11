1313b896ffe12c32f48719d0f1d33ae1c4a28d0a
Muirey03 <tcmuir03@gmail.com>
2025-03-11 15:02:41 +0000

    Track vm_object reference-counting primitives

--- tests/xnu/include/stubs.h
+++ tests/xnu/include/stubs.h
@@ -8,9 +8,20 @@
 #define CONFIG_ROSETTA 1
 
 #define THREAD_ENTRY __attribute__((annotate("thread_entrypoint")))
+#define TRACKED __attribute__((annotate("rc_ownership_tracked")))
+#define RETAINED __attribute__((annotate("rc_ownership_retained")))
+#define CONSUMED __attribute__((annotate("rc_ownership_consumed")))
 
 #define __builtin_xnu_type_signature(...) "12"
 #define __builtin_xnu_type_summary(...) 0
 #define __builtin_xnu_types_compatible(...) 1
+
+#if MACH_KERNEL_PRIVATE
+struct TRACKED vm_object;
+extern void vm_object_reference(RETAINED struct vm_object* object);
+extern void vm_object_reference_locked(RETAINED struct vm_object* object);
+extern void vm_object_reference_shared(RETAINED struct vm_object* object);
+extern void vm_object_deallocate(CONSUMED struct vm_object* object);
+#endif
 
 #endif
