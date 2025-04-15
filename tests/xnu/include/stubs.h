e7546f6211b7475db8d324bf6f1b524ea705ac14
Muirey03 <tcmuir03@gmail.com>
2025-04-15 18:27:13 +0100

    More entrypoints and tracked objects

--- tests/xnu/include/stubs.h
+++ tests/xnu/include/stubs.h
@@ -22,6 +22,31 @@ extern void vm_object_reference(RETAINED struct vm_object* object);
 extern void vm_object_reference_locked(RETAINED struct vm_object* object);
 extern void vm_object_reference_shared(RETAINED struct vm_object* object);
 extern void vm_object_deallocate(CONSUMED struct vm_object* object);
+
+struct TRACKED ipc_port;
+extern void ip_release(CONSUMED struct ipc_port*);
+extern void ip_reference(RETAINED struct ipc_port*);
+
+struct TRACKED task;
+extern void task_deallocate_grp(CONSUMED struct task*, uint32_t);
+extern void task_reference_grp(RETAINED struct task*, uint32_t);
+
+struct TRACKED ipc_voucher;
+extern void ipc_voucher_release(CONSUMED struct ipc_voucher*);
+extern void ipc_voucher_reference(RETAINED struct ipc_voucher*);
 #endif
+
+// TODO: struct socket
+
+struct TRACKED fsevent_handle;
+
+struct TRACKED ip_moptions;
+extern void imo_addref(RETAINED struct ip_moptions *imo, int locked);
+extern void imo_remref(CONSUMED struct ip_moptions *imo);
+
+struct TRACKED fileglob;
+struct proc;
+extern int fg_drop(struct proc* p, CONSUMED struct fileglob *fg);
+extern void fg_ref(struct proc* p, RETAINED struct fileglob *fg);
 
 #endif
